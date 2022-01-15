8a8231a053b571b9d0e1d82beb0f116518d9b276
Claudia Flores <cflores1@email.arizona.edu>
2022-01-15 15:29:03 -0700

    first commit on OOP

---
+++ concepts.js
@@ -0,0 +1,9 @@
+/* What is Object Orientend Programing?
+- relies on the concept of classes and objects
+- used to structure a software program into simple, 
+reusable pieces of code blueprints (usually called classes), 
+which are used to create individual instances of objects. 
+- Object-oriented programming languages include:
+ JavaScript, C++, Java, and Python
+
+*/
